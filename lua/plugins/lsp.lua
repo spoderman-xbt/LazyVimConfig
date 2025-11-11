ea81625cf3ef88b1ae12a042c698f773fb4f44f4
spoderman-xbt <233967958+spoderman-xbt@users.noreply.github.com>
2025-11-11 15:13:21 -0500

    Got Vue.js LSP support working but not sure which one of these changes exactly did it. :LazyExtras seemed to be the final change that made it work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,29 +1,29 @@
 return {
-  { "mason-org/mason-lspconfig.nvim", opts = { ensure_installed = { "vtsls", "vue_ls" } } },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          filetypes = { "typescript", "javascript", "vue" },
-          settings = {
-            vtsls = {
-              tsserver = {
-                globalPlugins = {
-                  {
-                    name = "@vue/typescript-plugin",
-                    location = vim.fn.expand "$MASON/packages/vue-language-server/node_modules/@vue/language-server",
-                    languages = { "vue" },
-                  },
-                },
-              },
-            },
-          },
-        },
-        vue_ls = {
-          init_options = { typescript = {} },
-        },
-      },
-    },
-  },
+  -- { "mason-org/mason-lspconfig.nvim", opts = { ensure_installed = { "vtsls", "vue_ls" } } },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       vtsls = {
+  --         filetypes = { "typescript", "javascript", "vue" },
+  --         settings = {
+  --           vtsls = {
+  --             tsserver = {
+  --               globalPlugins = {
+  --                 {
+  --                   name = "@vue/typescript-plugin",
+  --                   location = vim.fn.expand "$MASON/packages/vue-language-server/node_modules/@vue/language-server",
+  --                   languages = { "vue" },
+  --                 },
+  --               },
+  --             },
+  --           },
+  --         },
+  --       },
+  --       vue_ls = {
+  --         init_options = { typescript = {} },
+  --       },
+  --     },
+  --   },
+  -- },
 }
