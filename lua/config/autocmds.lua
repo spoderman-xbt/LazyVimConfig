356a9cc2a9fba41732a22430a1dd9415a7ac29f5
spoderman-xbt <233967958+spoderman-xbt@users.noreply.github.com>
2025-11-11 09:22:40 -0500

    Enable auto-save and disable auto-format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- Create autogroup for autosave
+local autosave_group = vim.api.nvim_create_augroup("AutoSave", { clear = true })
+
+-- Autosave on InsertLeave and TextChanged
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  group = autosave_group,
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    -- Only save if buffer has a filename and is modifiable
+    if vim.api.nvim_buf_get_name(buf) ~= "" and vim.bo[buf].modifiable then
+      vim.cmd("silent! write")
+    end
+  end,
+})
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -197,3 +197,14 @@ return {
     },
   },
 }
+
+-- save darcula colorscheme code
+-- return {
+--   { "xiantang/darcula-dark.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "darcula-dark",
+--     },
+--   },
+-- }
