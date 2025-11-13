67dcdf7fc51fc41936be44566aa368c8ab18c7fb
spoderman-xbt <233967958+spoderman-xbt@users.noreply.github.com>
2025-11-13 14:03:34 +0100

    Duplicate selected lines (Cmd+d in Jetbrains), also remove linting code to re-do it

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,25 +1,26 @@
 return {
 
-  -- Ruff for python
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      python = { "ruff_fix" },
-    },
-  },
-
+  -- -- Ruff for python
+  -- "stevearc/conform.nvim",
+  -- opts = {
+  --   formatters_by_ft = {
+  --     python = { "ruff_fix" },
+  --   },
+  -- },
+  --
   -- -- none-ls setup
   -- "nvimtools/none-ls.nvim",
   -- config = function()
   --   local null_ls = require("null-ls")
   --   null_ls.setup({
   --       sources = {
-  --           null_ls.builtins.formatting.stylua,
-  --           null_ls.builtins.completion.spell,
-  --           require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+  --           null_ls.builtins.diagnostics.mypy,
+  --           -- null_ls.builtins.formatting.stylua,
+  --           -- null_ls.builtins.completion.spell,
+  --           -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
   --       },
   --   })
-  --   vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+  --   -- vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
   -- end
 
 }
